8557a4ce73a8f49ffa37413a67368d7b5a09275b
akskinhikar <akskinhikar@gmail.com>
2025-03-05 17:44:07 -0500

    Added Log4j file and logging mechanism

--- src/main/java/com/qa/util/TestUtil.java
+++ src/main/java/com/qa/util/TestUtil.java
@@ -1,12 +1,12 @@
 package com.qa.util;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.*;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.io.File;
+import java.io.IOException;
 import java.time.Duration;
 
 public class TestUtil {
@@ -30,4 +30,27 @@ public class TestUtil {
         // Scrolling down the page till the element is found
         js.executeScript("arguments[0].scrollIntoView();", driver.findElement(elementName));
     }
+
+
+    public String get_screen_shot(String testCaseName) throws IOException {
+        TakesScreenshot ts = (TakesScreenshot) driver;
+        File source = ts.getScreenshotAs(OutputType.FILE);
+        File file = new File(System.getProperty("user.dir")+"//report//"+ testCaseName + ".png");
+        try {
+            FileUtils.copyFile(source, file);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        System.out.println("Screen shot saved successfully");
+        return System.getProperty("user.dir")+"//report//"+ testCaseName + ".png";
+    }
+
+    public String get_screen_shot_BASE(String testCaseName) {
+        TakesScreenshot ts = (TakesScreenshot) driver;
+        String base64Code = ts.getScreenshotAs(OutputType.BASE64);
+        System.out.println("Screen shot saved successfully");
+        return base64Code;
+    }
+
+
 }
--- src/test/java/com/qa/stepdefenation/LoginPageStepDefenation.java
+++ src/test/java/com/qa/stepdefenation/LoginPageStepDefenation.java
@@ -2,13 +2,16 @@ package com.qa.stepdefenation;
 
 import com.qa.factory.DriverFactory;
 import io.cucumber.java.en.Given;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class LoginPageStepDefenation {
-
+	public static Logger log = LogManager.getLogger(LoginPageStepDefenation.class);
 	@Given("^I have landed on salesforce page$")
 	public void i_have_landed_on_salesforce_page() {
 		try {
 			DriverFactory.getDriver().get("https://deloittesandbox-4b-dev-ed.develop.my.salesforce.com/");
+			log.info("Driver Initiated successfully with provided URL");
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
